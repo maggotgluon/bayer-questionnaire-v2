a1647166495b210432c48dc8317880710ba2d2f2
maggotgluon <maggotgluon@gmail.com>
2024-09-03 17:18:10 +0700

    add ga support

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -25,6 +25,18 @@
         @livewireChartsScripts
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          
+            ga('create', {{env('ANALYTICS_PROPERTY_ID')}}, 'auto');
+            ga('send', 'pageview');
+          
+          </script>
+
     </head>
 
     <body class="w-full min-h-screen grid justify-items-center">
